5ee83023736ab168e9641c9b15a6378f526aab78
David Gardner <dave@pixelkarma.com>
2024-08-19 23:50:21 -0700

    new exceptions

---
+++ src/Exceptions/RouteMiddlewareException.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Pixelkarma\PkRouter\Exceptions;
+
+use Exception;
+
+/**
+ * Class RouteMiddlewareException
+ * 
+ * This exception is thrown when an unhandled error occurs in a route middleware within the PkRouter.
+ * It extends the base Exception class and includes a default message, code,
+ * and support for exception chaining.
+ * 
+ * @package Pixelkarma\PkRouter\Exceptions
+ */
+class RouteMiddlewareException extends Exception {
+  
+  /**
+   * RouteMiddlewareException constructor.
+   * 
+   * @param string $message The Exception message to throw. Defaults to "Route middleware exception".
+   * @param int $code The Exception code. Defaults to 500.
+   * @param Exception|null $previous The previous exception used for exception chaining.
+   */
+  public function __construct($message = "Route middleware exception", $code = 500, Exception $previous = null) {
+    parent::__construct($message, $code, $previous);
+  }
+}
