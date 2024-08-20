54e38df94ac74764810f132da0696ee1daec371f
David Gardner <dave@pixelkarma.com>
2024-08-20 08:52:11 -0700

    indentation

--- src/PkRoutesConfig.php
+++ src/PkRoutesConfig.php
@@ -12,36 +12,36 @@ namespace Pixelkarma\PkRouter;
  */
 abstract class PkRoutesConfig {
 
-    /**
-     * @var array The list of routes defined in the application.
-     */
-    protected array $routeList = [];
+  /**
+   * @var array The list of routes defined in the application.
+   */
+  protected array $routeList = [];
 
-    /**
-     * PkRoutesConfig constructor.
-     *
-     * Initializes the route list by calling the abstract `routes()` method.
-     */
-    final public function __construct() {
-        $this->routeList = $this->routes();
-    }
+  /**
+   * PkRoutesConfig constructor.
+   *
+   * Initializes the route list by calling the abstract `routes()` method.
+   */
+  final public function __construct() {
+    $this->routeList = $this->routes();
+  }
 
-    /**
-     * Retrieves the list of routes.
-     *
-     * @return array The array of routes defined in the application.
-     */
-    final public function getRoutes() {
-        return $this->routeList;
-    }
+  /**
+   * Retrieves the list of routes.
+   *
+   * @return array The array of routes defined in the application.
+   */
+  final public function getRoutes() {
+    return $this->routeList;
+  }
 
-    /**
-     * Abstract method for defining routes.
-     *
-     * This method must be implemented by any class extending PkRoutesConfig.
-     * It should return an array of route definitions.
-     *
-     * @return array The array of route definitions.
-     */
-    abstract protected function routes();
+  /**
+   * Abstract method for defining routes.
+   *
+   * This method must be implemented by any class extending PkRoutesConfig.
+   * It should return an array of route definitions.
+   *
+   * @return array The array of route definitions.
+   */
+  abstract protected function routes();
 }
